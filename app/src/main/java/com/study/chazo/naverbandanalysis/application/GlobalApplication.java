df8fba020a0d40b38e642a2ba7120f18cbcd5726
Chazo <darker826@gmail.com>
2018-07-13 00:21:55 +0900

    import dagger2

---
+++ app/src/main/java/com/study/chazo/naverbandanalysis/application/GlobalApplication.java
@@ -0,0 +1,17 @@
+package com.study.chazo.naverbandanalysis.application;
+
+import android.app.Application;
+
+import com.study.chazo.naverbandanalysis.base.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class GlobalApplication extends DaggerApplication {
+
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
